7354dc33e3b628643f339a93febcdc1793e28e74
Menamonmon <monyasser111@gmail.com>
2021-02-17 09:19:15 -0500

    Users list now uses standart types from API

--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useUsers } from "../contexts/UsersContext";
 import UsersListItem from "./UsersListItem";
 
-export default function UsersList({ users, updateInvitedUser }) {
+export default function UsersList() {
+  let { activeUsers: users, updateInvitedUser } = useUsers();
+
   return (
     <li className="users-list">
       {users.map((user) => (
