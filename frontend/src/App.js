0650834ea256c392425a6757ce9cd2a4a36da36d
Menamonmon <monyasser111@gmail.com>
2021-02-22 12:44:50 -0500

    Added SocketProvider for SocketContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,24 @@ import Routes from "./components/Routes";
 import TypesProvider from "./contexts/TypesContext";
 import UsersProvider from "./contexts/UsersContext";
 import GamesProvider from "./contexts/GamesContext";
+import SocketProvider from "./contexts/SocketConext";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <TypesProvider>
-        <UsersProvider>
-          <GamesProvider>
-            <Router>
-              <Routes />
-            </Router>
-          </GamesProvider>
-        </UsersProvider>
-      </TypesProvider>
+      <SocketProvider>
+        <TypesProvider>
+          <UsersProvider>
+            <GamesProvider>
+              <Router>
+                <Routes />
+              </Router>
+            </GamesProvider>
+          </UsersProvider>
+        </TypesProvider>
+      </SocketProvider>
     </div>
   );
 }
