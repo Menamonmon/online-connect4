fb9f50d979ad849486e81c796eb8f901d8bdea74
Menamonmon <monyasser111@gmail.com>
2021-02-22 09:22:01 -0500

    removed default game & users values

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Routes from "./components/Routes";
@@ -10,34 +10,6 @@ import GamesProvider from "./contexts/GamesContext";
 import "./App.css";
 
 function App() {
-  const [game, setGame] = useState({
-    state: "000000000000000000000000000000000000",
-    status: "GAMENOTAVAILABLE",
-    id: 2312,
-    created_at: "2021-02-10 17:43:24.397221",
-    player_1_id: 203,
-    player_2_id: 2,
-    ended_at: null,
-    player_1_color: "2",
-    player_2_color: "1",
-    winner: null,
-    current_player: 203,
-  });
-
-  const currentUser = {
-    name: "Mena",
-    id: 203,
-    status: "USERUNAVAILABLE",
-    created_at: "2021-02-2 17:43:24.397221",
-  };
-
-  const invitedUser = {
-    name: "Masio",
-    id: 2,
-    status: "USERUNAVAILABLE",
-    created_at: "2021-02-3 17:43:24.397221",
-  };
-
   return (
     <div className="App">
       <TypesProvider>
