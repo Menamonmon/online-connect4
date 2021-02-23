6772f5accca80c355db2eb815ab73a2d993ffbad
Menamonmon <monyasser111@gmail.com>
2021-02-22 23:26:45 -0500

    setup middleware for authenticating users when connecting to socket + added router for socket-events endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,42 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { initializeDatabase } = require("./db/queries");
+const {
+  initializeDatabase,
+  users: { doesUserExist },
+} = require("./db/queries");
 const app = express();
 const httpServer = require("http").createServer(app);
 const socketIOOptions = {
   cors: {
     origin: "http://localhost:3000",
-  }
+  },
 };
+const socketEvents = require("./sockets/events");
+
 const io = require("socket.io")(httpServer, socketIOOptions);
 
-io.on("connection", socket => {
-  console.log("A CONNECTION HAS BEEN MADE");
-})
+io.use(async (socket, next) => {
+  // checking that this socket connection has a user
+  const { user } = socket.handshake.auth;
+  if (await doesUserExist(user)) {
+    socket.user = user;
+    next();
+  }
+
+  return next(new Error("invalid user object"));
+});
+
+io.on("connection", (socket) => {
+  console.log("USER CONNECTED");
+  // going through all of the users in the current socket and adding them to a list that would be updated for all the users
+  const activeUsers = [];
+  for (const [socket] of io.of("/").sockets) {
+    activeUsers.push(socket.user);
+  }
+
+  socket.emit(socketEvents.ACTIVE_USERS_UPDATED, activeUsers);
+});
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -26,6 +49,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/types", require("./routes/api/types"));
 app.use("/users", require("./routes/api/users"));
 app.use("/games", require("./routes/api/games"));
+app.use("/socket-events", require("./routes/api/socketEvents"));
 
 const PORT = process.env.APP_PORT;
 httpServer.listen(PORT, async () => {
