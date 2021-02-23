565635f0d9c025e4465fe186e719b43309cef80c
Menamonmon <monyasser111@gmail.com>
2021-02-22 23:29:49 -0500

    socket context now has the socket events object with it

--- frontend/src/contexts/SocketConext.js
+++ frontend/src/contexts/SocketConext.js
@@ -1,5 +1,6 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import api from "../requests/api";
 
 const URL = "http://localhost:5000";
 const socket = io(URL, { autoConnect: false });
@@ -7,8 +8,22 @@ const socket = io(URL, { autoConnect: false });
 const SocketContext = createContext();
 
 export default function SocketProvider({ children }) {
+  const [socketEvents, setSocketEvents] = useState({});
+  useEffect(() => {
+    async function fetchSocketEventsData() {
+      const socketEventsData = await api.getSocketEvents();
+      setSocketEvents(socketEventsData);
+    }
+
+    fetchSocketEventsData();
+  }, []);
   return (
-    <SocketContext.Provider value={socket}>
+    <SocketContext.Provider
+      value={{
+        socket,
+        socketEvents,
+      }}
+    >
       {children}
     </SocketContext.Provider>
   );
