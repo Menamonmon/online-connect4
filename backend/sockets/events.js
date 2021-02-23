0b5dda0d756773e3c69073e2aabb83cd1e6bdab8
Menamonmon <monyasser111@gmail.com>
2021-02-22 23:27:59 -0500

    a socket directory that will have events and things related to managing sockets

---
+++ backend/sockets/events.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ACITVE_USERS_UPDATED: "active users updated",
+  INVITATION_REQUEST: "invitation request",
+  INVITATION_REJECTION: "invitation rejection",
+  CREATE_GAME: "create game",
+  GAME_CREATED: "game created",
+};
