a6dd94f08bcb4073957b2ea139611307f4121c1f
Menamonmon <monyasser111@gmail.com>
2021-02-24 14:27:18 -0500

    renamed functions so that they match with UserContext

--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -3,14 +3,13 @@ import { useUsers } from "../contexts/UsersContext";
 import UsersListItem from "./UsersListItem";
 
 export default function UsersList() {
-  let { activeUsers: users, setInvitedUsers } = useUsers();
-
+  let { activeUsers: users, setInvitedUser } = useUsers();
   return (
     <li className="users-list">
       {users.map((user) => (
         <UsersListItem
           user={user}
-          onClick={() => setInvitedUsers(user)}
+          onClick={() => setInvitedUser(user)}
           key={`user-${user.id}`}
         />
       ))}
