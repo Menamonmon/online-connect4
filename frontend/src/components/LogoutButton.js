8a56d8d6e12332f081a256bb2380b221a60598cf
Menamonmon <monyasser111@gmail.com>
2021-02-24 19:41:42 -0500

    a logout-btn to end the socket connection and logout the user

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { useSocket } from "../contexts/SocketConext";
+import { useUsers } from "../contexts/UsersContext";
 
 export default function LogoutButton() {
   const { socket } = useSocket();
+  const { setCurrentUser } = useUsers();
+
   return (
-    <button onClick={() => socket.emit("logout")} className="logout-btn">
+    <button
+      onClick={() => {
+        setCurrentUser({});
+        socket.emit("logout");
+        socket.disconnect();
+      }}
+      className="logout-btn"
+    >
       Logout
     </button>
   );
