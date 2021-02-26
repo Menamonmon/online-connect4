fcf7216983cfa88c2c5bda86a5761df681e9d852
Menamonmon <monyasser111@gmail.com>
2021-02-26 09:35:06 -0500

    the list item now uses a button to trigger invitation

--- frontend/src/components/UsersListItem.js
+++ frontend/src/components/UsersListItem.js
@@ -17,26 +17,27 @@ const matchUserStatus = (status, types) => {
 
 export default function UserListItem({
   user: { name, status, created_at },
-  onClick,
+  onInvite,
 }) {
   const { user: userTypes } = useTypes();
   const createdAt = new Date(created_at);
   return (
-    <Link to="/invite-user" onClick={onClick}>
-      <div className="user-list-item">
-        <h3 className="user-name">{name}</h3>
-        <h4
-          className="user-status"
-          style={{
-            textTransform: "capitalize",
-          }}
-        >
-          {matchUserStatus(status, userTypes)}
-          <br />
-          Joined Since{" "}
-          {`${createdAt.getMonth()}/${createdAt.getDate()}/${createdAt.getFullYear()}`}
-        </h4>
-      </div>
-    </Link>
+    <div className="user-list-item">
+      <h3 className="user-name">{name}</h3>
+      <h4
+        className="user-status"
+        style={{
+          textTransform: "capitalize",
+        }}
+      >
+        {matchUserStatus(status, userTypes)}
+        <br />
+        Joined Since{" "}
+        {`${createdAt.getMonth()}/${createdAt.getDate()}/${createdAt.getFullYear()}`}
+      </h4>
+      <button className="invite-btn" onClick={onInvite}>
+        Invite To Game
+      </button>
+    </div>
   );
 }
