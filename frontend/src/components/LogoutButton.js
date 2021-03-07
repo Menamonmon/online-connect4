464d2a478bd632d69fccdb4e4a4bd6b0ec6449f1
Menamonmon <monyasser111@gmail.com>
2021-03-07 15:24:59 -0500

    now the only mechanism that the logout btn uses is disconnecting from the socket

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { useSocket } from "../contexts/SocketConext";
-import { useUsers } from "../contexts/UsersContext";
 
 export default function LogoutButton() {
   const { socket } = useSocket();
-  const { setCurrentUser } = useUsers();
 
   return (
     <button
       onClick={() => {
-        setCurrentUser({});
         socket.disconnect();
       }}
       className="logout-btn"
