30b2c9d5f8dad8af4cf8aeb3622074ba0220c908
Menamonmon <monyasser111@gmail.com>
2021-03-10 14:19:17 -0500

    removed link to game when user is logged in and replaced it with automatic redirect to game page

--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useUsers } from "../contexts/UsersContext";
 import { useGames } from "../contexts/GamesContext";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
 import UsersListItem from "./UsersListItem";
 import InviteUserModal from "./InviteUserModal";
 
@@ -11,30 +12,28 @@ export default function UsersList() {
   const { activeUsers: users, setInvitedUser } = useUsers();
   const { currentGame } = useGames();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (currentGame && !isObjectEmpty(currentGame)) {
+      history.push("/game");
+    }
+  }, [currentGame, history]);
 
   return (
     <div>
-      {!currentGame || isObjectEmpty(currentGame) ? (
-        <li className="users-list">
-          {users.map((user) => (
-            <UsersListItem
-              user={user}
-              onInvite={() => {
-                setInvitedUser(user);
-                setIsModalOpen(true);
-              }}
-              key={`user-${user.id}`}
-            />
-          ))}
-        </li>
-      ) : (
-        <>
-          <h1>
-            Please go back the game your are currently in and end it from there
-          </h1>
-          <Link to="/game">Go Back to Game</Link>
-        </>
-      )}
+      <li className="users-list">
+        {users.map((user) => (
+          <UsersListItem
+            user={user}
+            onInvite={() => {
+              setInvitedUser(user);
+              setIsModalOpen(true);
+            }}
+            key={`user-${user.id}`}
+          />
+        ))}
+      </li>
       <InviteUserModal isOpen={isModalOpen} setOpen={setIsModalOpen} />
     </div>
   );
