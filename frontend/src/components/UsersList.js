82d53fc908bb1210b79f8890a937b71477b8c8f0
Menamonmon <monyasser111@gmail.com>
2021-03-10 15:35:00 -0500

    Revert "removed link to game when user is logged in and replaced it with automatic redirect to game page"
    This reverts commit 30b2c9d5f8dad8af4cf8aeb3622074ba0220c908.

--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useUsers } from "../contexts/UsersContext";
 import { useGames } from "../contexts/GamesContext";
-import { useHistory } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import UsersListItem from "./UsersListItem";
 import InviteUserModal from "./InviteUserModal";
 
@@ -12,28 +11,30 @@ export default function UsersList() {
   const { activeUsers: users, setInvitedUser } = useUsers();
   const { currentGame } = useGames();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const history = useHistory();
-
-  useEffect(() => {
-    if (currentGame && !isObjectEmpty(currentGame)) {
-      history.push("/game");
-    }
-  }, [currentGame, history]);
 
   return (
     <div>
-      <li className="users-list">
-        {users.map((user) => (
-          <UsersListItem
-            user={user}
-            onInvite={() => {
-              setInvitedUser(user);
-              setIsModalOpen(true);
-            }}
-            key={`user-${user.id}`}
-          />
-        ))}
-      </li>
+      {!currentGame || isObjectEmpty(currentGame) ? (
+        <li className="users-list">
+          {users.map((user) => (
+            <UsersListItem
+              user={user}
+              onInvite={() => {
+                setInvitedUser(user);
+                setIsModalOpen(true);
+              }}
+              key={`user-${user.id}`}
+            />
+          ))}
+        </li>
+      ) : (
+        <>
+          <h1>
+            Please go back the game your are currently in and end it from there
+          </h1>
+          <Link to="/game">Go Back to Game</Link>
+        </>
+      )}
       <InviteUserModal isOpen={isModalOpen} setOpen={setIsModalOpen} />
     </div>
   );
