b0d498c00b386a446e70b15c2e31df79b3df9afc
Menamonmon <monyasser111@gmail.com>
2021-03-10 15:33:46 -0500

    end game event is now passed a copy of the final game

--- frontend/src/components/LeaveGameButton.js
+++ frontend/src/components/LeaveGameButton.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useGames } from "../contexts/GamesContext";
 import { useSocket } from "../contexts/SocketConext";
 
 export default function LeaveGameButton() {
   const { socket } = useSocket();
+  const { currentGame } = useGames();
 
-  return <button onClick={() => socket.emit("end game")}>Leave Game</button>;
+  return (
+    <button onClick={() => socket.emit("end game", currentGame)}>
+      Leave Game
+    </button>
+  );
 }
