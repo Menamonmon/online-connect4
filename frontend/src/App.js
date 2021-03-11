5f32982a9f44d8a172c25a4fd50cbba3b7c24d83
Menamonmon <monyasser111@gmail.com>
2021-03-11 01:00:28 -0500

    changed router to HashRouter to be compatible with gh-pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import Routes from "./components/Routes";
 
