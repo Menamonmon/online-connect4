4dd2ecc7fc206dd80826e183d11e660fdcea3a75
menamonmon <monyasser111@gmail.com>
2021-05-22 10:49:08 -0400

    changed debug url to IP addr instead of localhost

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -1,6 +1,6 @@
 const DEBUG = process.env.REACT_APP_DEBUG === "true";
 const baseURL = DEBUG
-  ? "http://localhost:5000"
+  ? "http://10.0.0.218:5000"
   : "https://online-connect4.herokuapp.com";
 
 export { baseURL };
